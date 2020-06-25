645c9fc85e6dd7d0b0baf16555073676372b0b0e
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2020-06-24 22:00:11 -0400

    projectlist class added

--- src/app.js
+++ src/app.js
@@ -38,6 +38,24 @@ function autobind(_, _2, descriptor) {
 }
 //ProjectList Class
 class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importNode = document.importNode(this.templateElement.content, true);
+        this.element = importNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
 }
 //ProjectInput Class
 class ProjectInput {
@@ -109,3 +127,5 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+
+
+
+
 //Validation
 interface Validatable {
     value: string | number;
@@ -46,7 +50,24 @@ function autobind(_:any,_2:string,descriptor: PropertyDescriptor){
 class ProjectList {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
-    element
+    element : HTMLElement;
+    constructor(private type: 'active' | 'finished'){
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-list')!;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        const importNode = document.importNode(this.templateElement.content,true);
+        this.element = importNode.firstElementChild as HTMLFormElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    private renderContent(){
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase()+' PROJECTS';
+    }
+    private attach(){
+        this.hostElement.insertAdjacentElement('beforeend',this.element);
+    }
 }
 
 //ProjectInput Class
@@ -127,3 +148,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
